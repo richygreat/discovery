aa8374b872d583d542bc82547ab5423e863b52e5
Richy Great <richygreat@gmail.com>
2018-04-11 08:59:44 +0530

    Update server_start.sh

--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
 cd /home/ec2-user/discovery
-sudo /usr/bin/java -jar -Xmx128m -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+
+sudo chmod 777 discovery-0.0.0.jar
+sudo ln -s /home/ec2-user/discovery/discovery-0.0.0.jar /etc/init.d/discovery
+sudo touch /var/log/discovery.log
+sudo chmod 777 /var/log/discovery.log
+sudo service discovery start
