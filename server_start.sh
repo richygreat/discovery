390ba2300ba5a602775c0e91805cc0a3dfaeb1af
Richy Great <richygreat@gmail.com>
2018-04-11 09:39:01 +0530

    Update server_start.sh

--- server_start.sh
+++ server_start.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user/discovery
 sudo chmod 777 *.sh
 sudo chmod 777 discovery-0.0.0.jar
 sudo ln -s /home/ec2-user/discovery/discovery-0.0.0.jar /etc/init.d/discovery
-update-rc.d discovery defaults
+sudo chkconfig discover on
 sudo touch /var/log/discovery.log
 sudo chmod 777 /var/log/discovery.log
 sudo service discovery start
